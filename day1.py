15b422964d6753a1377a78d548472ac4d35e17ed
Shivam Mishra <shivam1602m@gmail.com>
2024-06-16 12:47:06 +0530

    Day 1 completed and commented. Day 1 project also updated

--- day1.py
+++ day1.py
@@ -1,19 +1,28 @@
 print("Day 1 - Python Print Function \nThe function is declared like this: \nprint('what to print')\n")
 
+#Getting spaces in output
 print("Hello"+" Shivam")
 print("Hello " + "Shivam")
 print("Hello", "Shivam \n")
 
+#Code debugging
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the '+' sign.")
 print('e.g. print("Hello"+"World")')
 print("New lined can be created with a backlash and n \n")
 
+#Comments
 #print("Hello "+input("What is your name ") + "!")
 
 #This is a comment
 """
 This is multi line comment
 """
+#Get used to len in inputs
+print(len(input("What is your name? \n")))
 
-print(len(input("What is your name? ")))
+#Swap numbers
+a=input("a:")
+b=input("b:")
+a,b=b,a
+print("a:",a,"b:",b)
---
+++ day1_project_band_name_generator.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program 
+print("Hello user, welcome to band name generator.\n")
+#2. Ask the user for city that they grew up in 
+city=input("Which city did you grow up in? \n")
+#3. Ask the user for the name of a pet 
+pet_name=input("What is your pet name? \n")
+#4. Combine the name of city and pet and show them their band name
+print("Your band name is",city,pet_name)
+#5. Make sure the input cursor shows on a new line.
