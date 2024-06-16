1402f1bca32d13b4eabe354a74cae8ae9593c70d
Shivam Mishra <shivam1602m@gmail.com>
2024-06-16 11:59:33 +0530

    Day 1 completed

--- day1.py
+++ day1.py
@@ -1,1 +1,13 @@
-print("Day 1 - Python Print Function \nThe function is declared like this: \nprint('what to print')")
+print("Day 1 - Python Print Function \nThe function is declared like this: \nprint('what to print')\n")
+
+print("Hello"+" Shivam")
+print("Hello " + "Shivam")
+print("Hello", "Shivam \n")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello"+"World")')
+print("New lined can be created with a backlash and n \n")
+
+print("Hello "+input("What is your name ") + "!")
+
