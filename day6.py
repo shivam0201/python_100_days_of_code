100d5cf636a3f5ba06c4659fdc1a78c505300c78
Shivam Mishra <shivam1602m@gmail.com>
2024-07-01 12:33:39 +0530

    All the excercises were supposed to be done in the website, cant help much

---
+++ day6.py
@@ -0,0 +1,13 @@
+#Functions and Indentation
+print("Hello")
+num_char=len("Hello")
+print(num_char)
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+#Indentation
+# tab vs spaces debate
+#While loop and infinite while loops
